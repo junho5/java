dff5b57fd7d1e7eb3e1a97b41539b936afde3721
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-07-28 01:12:53 +0900

    'first'

---
+++ Coding_Test/src/Level_2/Main.java
@@ -0,0 +1,31 @@
+package Level_2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int result = 0;
+		while(true) {
+			if (num % 5 == 0) {
+				result += num / 5;
+				break;
+			}
+			else {
+				num -= 3;
+				result += 1;
+			}
+			
+			if (num < 0) {
+				result = -1;
+				break;
+			}
+			
+		}
+		System.out.println(result);
+	}
+
+}
