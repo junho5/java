2458dcb78561193a066293ba2d1df1b7b6042fdb
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-07-29 21:58:54 +0900

    0729

--- Coding_Test/src/Backjoon/Silver_2839.java
+++ Coding_Test/src/Backjoon/Silver_2839.java
@@ -1,8 +1,8 @@
-package Level_2;
+package Backjoon;
 
 import java.util.Scanner;
 
-public class Sugar {
+public class Silver_2839 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
