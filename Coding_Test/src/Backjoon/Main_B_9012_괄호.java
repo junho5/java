3414cf4838bbdd1dd10ee3949284d09148f58b55
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-08-30 23:29:25 +0900

    todat commit

--- Coding_Test/src/Backjoon/Main_B_9012_괄호.java
+++ Coding_Test/src/Backjoon/Main_B_9012_괄호.java
@@ -15,50 +15,30 @@ public class Main_B_9012_괄호 {
 		int tc = Integer.parseInt(br.readLine());
 		
 		for (int i = 0; i < tc; i++) {
+			boolean flag = true;
 			Stack<Character> s = new Stack<>();
 			char [] arr = br.readLine().toCharArray();
 			
 			for (int j = 0; j < arr.length; j++) {
-				s.push(arr[j]);
-			}
-			
-//			System.out.println(s);
-			
-			boolean flag = true;
-			int index = 1;
-			int check = 0;
-			
-			while(!s.isEmpty()) {
-				char temp = s.pop();
-				
-				if (check < 0) {
-					flag = false;
-					break;
-				}
-				
-				if (index == 1 && temp =='(') {
+				if (arr[j] == '(') {
+					s.push(arr[j]);
+				}else if (arr[j] == ')' && s.size() > 0) {
+					s.pop();
+				}else {
 					flag = false; 
-					break;
 				}
-				
-				if (temp == ')') {
-					check++; 
-					index++;
-				}
-				
-				if (temp == '(') {
-					check--;
-					index++;
-					if (check > 0 && s.peek() == ')') {
-						flag = false;
-						break;
-					}
-				}
-				
 			}
 			
-			System.out.println(flag);
+			int answer  = s.size();
 			
+			if (flag == false) {
+				System.out.println("NO");
+			}
+			else if (flag == true && answer > 0) {
+				System.out.println("NO");
+			}else {
+				System.out.println("YES");
+			}
 			
 			
 		}
