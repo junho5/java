f58c2095b62d06785c1040e126767b4a8e5ac9ca
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-09-07 22:24:36 +0900

    0907

--- Coding_Test/src/Backjoon/Main_B_4949_균형잡힌세상.java
+++ Coding_Test/src/Backjoon/Main_B_4949_균형잡힌세상.java
@@ -1,70 +1,79 @@
 package Backjoon;
 
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+ 
 public class Main_B_4949_균형잡힌세상 {
-
-	static int a,b;
-	
-	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("input/4949.txt"));
+ 
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
+ 
+		String s;
 		
 		while(true) {
-			Stack<Character> stack = new Stack<>();
-			boolean flag = true;
-			int index = 0;
-			String word = br.readLine();
-			if (word.equals(".") && word.length() == 1) break;
 			
-			word = word.replaceAll(" ", "");
-//			System.out.println(word);
-			a = 0;
-			b = 0;
-			for (int i = 0; i < word.length(); i++) {
-				if (word.charAt(i) == '(') {
-					stack.push(word.charAt(i));
-					index = 0;
-					a++;
-				}else if (word.charAt(i) == ')') {
-					if (stack.size()<=0 || a <= 0 || index == -1) {
-						flag =  false;
-						break;
-					}else {
-						stack.pop();
-					}
-				}else if (word.charAt(i) == '[') {
-					stack.push(word.charAt(i));
-					index = -1;
-					b++;
-				}else if (word.charAt(i) == ']') {
-					if (stack.size()<=0 || b <= 0 || index == 0) {
-						flag =  false;
-						break;
-					}else {
-						stack.pop();
-					}
+			s = br.readLine();
+			
+			if(s.equals(".")) {	// 종료 조건문 
+				break;
+			}
+			
+			sb.append(solve(s)).append('\n');
+		}
+		
+		System.out.println(sb);
+		
+		
+	}
+	
+	public static String solve(String s) {
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i = 0; i < s.length(); i++) {
+			
+			char c = s.charAt(i);	// i 번째 문자 
+			
+			// 여는 괄호일 경우 스택에 push 
+			if(c == '(' || c == '[') {
+				stack.push(c);
+			}
+			
+			// 닫는 소괄호 일 경우 
+			else if(c == ')') {
+				
+				// 스택이 비어있거나 pop할 원소가 소괄호랑 매칭이 안되는 경우 
+				if(stack.empty() || stack.peek() != '(') {
+					return "no";
+				}
+				else {
+					stack.pop();
 				}
 			}
 			
-			if (flag == false) {
-				System.out.println("no");
-			}else if(stack.size() == 0){
-				System.out.println("yes");
-			}else {
-				System.out.println("no");
+			else if(c == ']') {
+				
+				// 스택이 비어있거나 pop할 원소가 대괄호랑 매칭이 안되는 경우 
+				if(stack.empty() || stack.peek() != '[') {
+					return "no";
+				}
+				else {
+					stack.pop();
+				}
 			}
 			
-			
-			
+			// 그 외의 경우에는 불필요한 문자들이기에 skip한다. 
 		}
 		
-
+		if(stack.empty()) {
+			return "yes";
+		}
+		else {
+			return "no";
+		}
 	}
+ 
 }
-
-
-
