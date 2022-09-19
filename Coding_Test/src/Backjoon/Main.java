1ab48fcc01c03da622e214a7336ee7f7a639f4b1
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-09-19 22:15:36 +0900

    0919

--- Coding_Test/src/Backjoon/Main.java
+++ Coding_Test/src/Backjoon/Main.java
@@ -8,9 +8,38 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
-		System.out.println(Integer.parseInt("1F7",16));
-		System.out.println(Integer.parseInt("d",16));
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		Set<String> set = new HashSet<>();
+		Set<String> answer = new HashSet<>();
+		
+		for (int i = 0; i < a; i++) {
+			String temp = br.readLine();
+			set.add(temp);
+		}
+		
+		for (int i = 0; i < b; i++) {
+			String temp = br.readLine();
+			if (set.contains(temp)) {
+				answer.add(temp);
+			}
+		}
+		
+		List<String> list = new ArrayList<String>(answer);
+		
+		Collections.sort(list);
+		
+		sb.append(answer.size());
+		sb.append("\n");
+		for (String data : list) {
+			sb.append(data);
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
 		
 	}
 
