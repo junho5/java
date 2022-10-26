c7dddc68fe90939d156a871778856c7b8ffed062
Oh JUN Ho <dhwnsghdhkd@naver.com>
2022-10-26 23:40:12 +0900

    1026

---
+++ Coding_Test/src/A_Test/Main.java
@@ -0,0 +1,19 @@
+package A_Test;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("input/.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		
+	}
+
+
+}
