7f41a1ebe2988c6d0d558939bfc5a88c095e1f88
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-10-30 00:00:10 +0900

    1030

---
+++ Coding_Test/src/A_Test/Main_B_20055_컨베이어벨트위의로봇.java
@@ -0,0 +1,114 @@
+package A_Test;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_B_20055_컨베이어벨트위의로봇 {
+
+	static int N, K, ans, time;
+	static int start, end;
+	static int[] belt;
+	static boolean[] robot;
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input/20055.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 벨트 길이
+		K = Integer.parseInt(st.nextToken()); // 제한 수
+		ans = 0;
+		time = 0;
+
+		belt = new int[2 * N];
+		robot = new boolean[2 * N];
+		start = 0; // 올리는 위치
+		end = N - 1; // 내리는 위치
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < belt.length; i++) {
+			belt[i] = Integer.parseInt(st.nextToken());
+		}
+
+		while (true) {
+			time++;
+			move_belt(belt, robot); // 벨트랑 로봇 이동
+			drop(robot); // 로봇 drop 위치에 있으면 제거
+			move_robot(robot); // 로봇 움직이기
+			drop(robot); // 로봇 drop 위치에 있으면 제거
+			insert(); // 로봇 올리기
+			
+			// K개수 이상이면 중지
+			if (zero_check(belt))
+				break;
+		}
+		System.out.println(time);
+
+	}
+	
+	// 0 개수 체크해서 K개 이상이면 true 아니면 false 반환 함수
+	private static boolean zero_check(int[] belt) {
+		int cnt = 0;
+		for (int i = 0; i < belt.length; i++) {
+			if (belt[i] == 0) {
+				cnt++;
+			}
+		}
+		if (cnt >= K) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// 로봇 내리는 위치에 있으면 boolean값 변경 함수
+	private static void drop(boolean[] robot) {
+		if (robot[end] == true) {
+			robot[end] = false;
+		}
+	}
+	
+	// 로봇 벨트위에 올리고 내구도 감소 함수
+	private static void insert() {
+		if (robot[start] == false && belt[start] > 0) {
+			robot[start] = true;
+			belt[start]--;
+		}
+	}
+	
+	// 로봇 움직이는 함수
+	private static void move_robot(boolean[] robot) {
+		for (int i = robot.length - 1; i >= 0; i--) {
+			if (robot[i] == true) {
+				if (i == robot.length - 1) { // 마지막에 로봇이 위치하면
+					if (robot[0] == false && belt[0] > 0) {
+						belt[0]--;
+						robot[i] = false;
+						robot[0] = true;
+					}
+				} else { // 그 외
+					if (robot[i + 1] == false && belt[i + 1] > 0) {
+						belt[i + 1]--;
+						robot[i] = false;
+						robot[i + 1] = true;
+					}
+				}
+			}
+		}
+	}
+	
+	// 벨트 움직이는 함수 ( 벨트 , 로봇 동시에 움직임 )
+	private static void move_belt(int[] belt, boolean[] robot) {
+		int temp_blet = belt[belt.length - 1];
+		boolean temp_robot = robot[robot.length - 1];
+		for (int i = belt.length - 1; i > 0; i--) {
+			belt[i] = belt[i - 1];
+			robot[i] = robot[i - 1];
+		}
+		belt[0] = temp_blet;
+		robot[0] = temp_robot;
+	}
+
+}
--- Coding_Test/src/frame/Test.java
+++ Coding_Test/src/frame/Test.java
@@ -6,40 +6,22 @@ import java.util.StringTokenizer;
 
 public class Test {
 	
-	static int N;
-	static boolean[] canCheck = new boolean[40001];
 
     public static void main(String[] args) throws IOException{
-    	System.setIn(new FileInputStream("input/2629.txt"));
+//    	System.setIn(new FileInputStream("input/2629.txt"));
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         
-        N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 8; i++) {
+        	if (i == 2) {
+        		System.out.println(i);
+        		i++;
+        	}else {
+        		System.out.println(i);
+        	}
+		}
         
-        for (int i = 0; i < N; i++) {
-            int choo = Integer.parseInt(st.nextToken());
-            boolean[] tempCanCheck = new boolean[40001];
-            for (int j = 0; j < 40001; j++) {
-                if (canCheck[j]) {
-                    tempCanCheck[j] = true;
-                    tempCanCheck[Math.abs(choo-j)] = true;
-                    tempCanCheck[choo+j] = true; // 범위 안 벗어남: 500 * 30 < 40000
-                }
-            }
-            tempCanCheck[choo] = true;
-
-            canCheck = tempCanCheck;
-        }
-
-        int T = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < T; i++) {
-
-            sb.append(canCheck[Integer.parseInt(st.nextToken())]? "Y ": "N ");
-        }
-        System.out.println(sb);
     }
 
 }
