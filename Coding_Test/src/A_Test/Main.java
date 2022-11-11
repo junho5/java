729ae0ee20d8a76aa842d4ae8b6988f30fe9d490
junho5 <dhwnsghdhkd@naver.com>
2022-11-11 17:54:05 +0900

    1111

--- Coding_Test/src/A_Test/Main.java
+++ Coding_Test/src/A_Test/Main.java
@@ -5,41 +5,58 @@ import java.io.*;
 
 public class Main {
 	
-	static int x,y;
-	static int [][] map;
+	static int tc;
+	static int num,height;
+	static Integer [] arr;
+	static boolean [] v;
 
 	public static void main(String[] args) throws Exception{
-		System.setIn(new FileInputStream("input/2206.txt"));
+		System.setIn(new FileInputStream("input/1486.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
-		st = new StringTokenizer(br.readLine());
+		tc = Integer.parseInt(br.readLine());
 		
-		x = Integer.parseInt(st.nextToken());
-		y = Integer.parseInt(st.nextToken());
-		map = new int [x][y];
-		
-		
-		for (int i = 0; i < x; i++) {
-			String row = br.readLine();
-			for (int j = 0; j < y; j++) {
-				map[i][j] = row.charAt(j)-'0';
+		for (int test_case = 0; test_case < tc; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			num = Integer.parseInt(st.nextToken());
+			height = Integer.parseInt(st.nextToken());
+			v = new boolean[num];
+			
+			arr = new Integer[num];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
+			
+			Arrays.sort(arr, Collections.reverseOrder());
+			System.out.println(Arrays.toString(arr));
+			
+			subset(0);
 		}
 		
-		print(map);
 		
 	}
 
-	private static void print(int[][] map) {
-		for (int [] data_arr : map) {
-			for (int data : data_arr) {
-				System.out.printf("%d ",data);
+	private static void subset(int index) {
+		if (index == num) {
+			for (int i = 0; i < num; i++) {
+				if (v[i] == true) {
+					System.out.println("true");
+				}else {
+					System.out.println("false");
+				}
 			}
-			System.out.println();
+			return;
 		}
+		
+		v[index] = true;
+		subset(index + 1);
+		v[index] = false;
+		subset(index + 1);
 	}
+
 
 
 }
