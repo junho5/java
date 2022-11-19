9a9ba21084fd16a589030731492c119990efd404
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-11-19 13:11:36 +0900

    update

--- Coding_Test/src/A_Test/Solution_2112_보호필름.java
+++ Coding_Test/src/A_Test/Solution_2112_보호필름.java
@@ -6,9 +6,11 @@ import java.io.*;
 public class Solution_2112_보호필름 {
 	
 	static int tc;
+	static int x,y,k;
+	static int [][] film;
 
 	public static void main(String[] args) throws Exception{
-		System.setIn(new FileInputStream("input/.txt"));
+		System.setIn(new FileInputStream("input/2112.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
@@ -17,15 +19,32 @@ public class Solution_2112_보호필름 {
 		
 		for (int test_case = 1; test_case <= tc; test_case++) {
 			
+			st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
+			
+			film = new int [x][y];
+			
+			print(film);
 			
 			
 			
 			
-			sb.append("#");
-			sb.append(test_case);
-			sb.append(" ");
+//			sb.append("#");
+//			sb.append(test_case);
+//			sb.append(" ");
 		}
-		System.out.println(sb.toString());
+//		System.out.println(sb.toString());
+	}
+
+	private static void print(int[][] film) {
+		for (int [] data_arr : film) {
+			for (int data : data_arr) {
+				System.out.printf("%d ",data);
+			}
+			System.out.println();
+		}
 	}
 
 
