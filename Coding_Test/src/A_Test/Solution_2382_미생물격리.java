7cfbafe1258ca6ab8028a650b2f22de6b5945edd
Oh JUN Ho <dhwnsghdhkd@naver.com>
2022-11-21 23:48:32 +0900

    update

--- Coding_Test/src/A_Test/Solution_2382_미생물격리.java
+++ Coding_Test/src/A_Test/Solution_2382_미생물격리.java
@@ -6,9 +6,32 @@ import java.io.*;
 public class Solution_2382_미생물격리 {
 	
 	static int tc;
+	static int n,time,num;
+	static cell [][] map;
+	static boolean [][] v;
+	static PriorityQueue<cell> q = new PriorityQueue<>();
+	static int [] dx = {0,-1,1,0,0};
+	static int [] dy = {0,0,0,-1,1};
+	static class cell implements Comparable<cell>{
+		int x,y,life,dir;
+
+		public cell(int x, int y, int life, int dir) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.life = life;
+			this.dir = dir;
+		}
+
+		@Override
+		public int compareTo(cell o) {
+			return o.life - this.life;
+		}
+		
+	}
 
 	public static void main(String[] args) throws Exception{
-		System.setIn(new FileInputStream("input/.txt"));
+		System.setIn(new FileInputStream("input/2382.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
@@ -17,15 +40,93 @@ public class Solution_2382_미생물격리 {
 		
 		for (int test_case = 1; test_case <= tc; test_case++) {
 			
+			st = new StringTokenizer(br.readLine());
+			
+			n = Integer.parseInt(st.nextToken()); // 가로 세로 길이 
+			time = Integer.parseInt(st.nextToken()); // 격리 시간 
+			num = Integer.parseInt(st.nextToken()); // 군집 개수  
+			
+			map = new cell [n][n];
+			v = new boolean [n][n];
+			
+			for (int i = 0; i < num; i++) {
+				st = new StringTokenizer(br.readLine());
+				q.add(new cell(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+			}
+			
+			play();
+			print(map);
+//			System.out.println(cal());
 			
 			
-			
-			
-			sb.append("#");
-			sb.append(test_case);
-			sb.append(" ");
+//			sb.append("#");
+//			sb.append(test_case);
+//			sb.append(" ");
 		}
-		System.out.println(sb.toString());
+//		System.out.println(sb.toString());
+	}
+
+	private static void print(cell[][] map) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] != null) {
+					System.out.printf("%3d" , map[i][j].life);
+				}else {
+					System.out.printf("%3d" , 0);
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	private static int cal() {
+		int total = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] != null) {
+					total += map[i][j].life;
+				}
+			}
+		}
+		return total;
+	}
+
+	private static void play() {
+		while(time >= 0) {
+			
+			while(!q.isEmpty()) {
+				cell temp = q.poll();
+				// 셀 이동 
+				int mx = temp.x + dx[temp.dir];
+				int my = temp.y + dy[temp.dir];
+				
+				// 끝에 위치 할 경우 
+				if (mx == 0 || mx ==n-1 || my == 0 || my ==n-1) {
+					int life = temp.life;
+					life /= 2;
+					if (life == 0) continue;
+				
+//						끝 위치하므로 변경 상(1) 하(2) 좌(3) 우(4)
+						if (temp.dir == 1 || temp.dir == 3) temp.dir++;
+						else{
+							temp.dir--;
+						}
+				}
+				
+				if (map[mx][my] == null) {
+					map[mx][my] = new cell(mx, my, temp.life, temp.dir);
+					map[mx][my] = temp;
+				}else {
+					map[mx][my].life += temp.life;
+				}
+				
+			}
+			time--;
+//			System.out.println("====================");
+//			System.out.printf("%d\n",time);
+//			print(map);
+			
+		}
 	}
 
 
--- Coding_Test/src/A_Test/Solution_4193_수영대회결승전.java
+++ Coding_Test/src/A_Test/Solution_4193_수영대회결승전.java
@@ -6,9 +6,25 @@ import java.io.*;
 public class Solution_4193_수영대회결승전 {
 	
 	static int tc;
+	static int n;
+	static int [][] map;
+	static boolean [][] v;
+	static int sx,sy,ex,ey;
+	static int [] dx = {-1,1,0,0};
+	static int [] dy = {0,0,-1,1};
+	
+	static class node{
+		int x,y;
+		public node(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+		
+	}
 
 	public static void main(String[] args) throws Exception{
-		System.setIn(new FileInputStream("input/.txt"));
+		System.setIn(new FileInputStream("input/4193.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
@@ -17,15 +33,99 @@ public class Solution_4193_수영대회결승전 {
 		
 		for (int test_case = 1; test_case <= tc; test_case++) {
 			
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			
+			map = new int [n][n];
+			v = new boolean [n][n];
+			
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j] == 2) map[i][j] = 12;
+				}
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			sx = Integer.parseInt(st.nextToken()); // 시작 x
+			sy = Integer.parseInt(st.nextToken()); // 시작 y
+			st = new StringTokenizer(br.readLine());
+			ex = Integer.parseInt(st.nextToken()); // 끝 x
+			ey = Integer.parseInt(st.nextToken()); // 끝 y
 			
 			
+//			print(map);
 			
 			
 			sb.append("#");
 			sb.append(test_case);
 			sb.append(" ");
+			sb.append(bfs(sx,sy));
+			sb.append("\n");
 		}
 		System.out.println(sb.toString());
+	}
+
+	private static int bfs(int sx, int sy) {
+		Queue<node> q = new LinkedList<>();
+		q.add(new node(sx, sy));
+		v[sx][sy] = true;
+		
+		int time = 0;
+		while (!q.isEmpty()) {
+			int size = q.size();
+			
+			for (int i = 0; i < size; i++) {
+				node temp = q.poll();
+				if (temp.x == ex && temp.y == ey) return time;
+				
+				for (int j = 0; j < dx.length; j++) {
+					int mx = temp.x + dx[j];
+					int my = temp.y + dy[j];
+					
+					if (mx < 0 || my < 0 || mx >= n || my >= n) continue;
+					if (v[mx][my]==true || map[mx][my] == 1) continue;
+					
+					if (map[mx][my] == 0 || map[mx][my] == 10) {
+						v[mx][my] = true;
+						q.add(new node(mx, my));
+					}else {
+						q.add(new node(temp.x, temp.y));
+					}
+					
+				}
+				
+			}
+			time++;
+			process();
+			
+		}
+		
+		
+		return -1;
+		
+	}
+
+	private static void process() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] > 10) {
+					map[i][j]--;
+				}else if (map[i][j] == 10) {
+					map[i][j] = 12;
+				}
+			}
+		}
+	}
+
+	private static void print(int[][] map) {
+		for (int [] data_arr : map) {
+			for (int data : data_arr) {
+				System.out.printf("%3d ",data);
+			}
+			System.out.println();
+		}
 	}
 
 
