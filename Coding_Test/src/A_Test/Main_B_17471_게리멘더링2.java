fe81886f7dce94834655f9635336bbf398bbfe13
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-11-26 23:56:23 +0900

    1126

---
+++ Coding_Test/src/A_Test/Main_B_17471_게리멘더링2.java
@@ -0,0 +1,20 @@
+package A_Test;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_B_17471_게리멘더링2 {
+	
+
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("input/17471.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		
+		
+	}
+
+
+}
