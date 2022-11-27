ab5b6cb3b247a5d9bf7c0beeb605540baf42cc95
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-11-27 23:55:44 +0900

    1127

--- Coding_Test/src/A_Test/Main_B_17471_게리맨더링.java
+++ Coding_Test/src/A_Test/Main_B_17471_게리맨더링.java
@@ -8,13 +8,12 @@ public class Main_B_17471_게리맨더링 {
 	static int N,ans;
 	static int t_sum,f_sum;
 	static int [] population;
-	static int [][] map;
+	static int [][] graph;
 	static boolean [] V;
-	static boolean [] S_V;
-	static List<Integer> t_list;
-	static List<Integer> f_list;
+	static boolean [] D_V;
+	static List<Integer> t_list,f_list;
 	static Set<Integer> con;
-	static boolean flag,check_f;
+	static boolean flag;
 
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("input/17471.txt"));
@@ -24,25 +23,26 @@ public class Main_B_17471_게리맨더링 {
 		
 		N = Integer.parseInt(br.readLine()); // 정점 개수
 		population = new int [N]; // 인구수 배열
-		map = new int [N+1][N+1]; // 인접행렬
-		S_V = new boolean [N]; // 부분집합 방문 배열
+		graph = new int [N+1][N+1]; // 인접행렬
+		D_V = new boolean [N]; // 부분집합 방문 배열
+		flag = false; // 연결상태 확인 변수
 		ans = Integer.MAX_VALUE; // 답 담을 변수
+		
 		st = new StringTokenizer(br.readLine()); 
 		
-		for (int i = 0; i < N; i++) { // 인구수 배열에 입력 0 ~~~~ N
+		for (int i = 0; i < N; i++) { 
 			population[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine()); // 인접행렬 생성
-			int adj = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < adj; j++) {
-				int gu = Integer.parseInt(st.nextToken());
-				map[i][gu] = 1;
+			int range = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < range; j++) {
+				graph[i][Integer.parseInt(st.nextToken())] = 1;
 			}
 		}
 		subset(0); 
-		if (check_f) {
+		if (flag) {
 			System.out.println(ans);
 		}else {
 			System.out.println(-1);
@@ -58,77 +58,63 @@ public class Main_B_17471_게리맨더링 {
 			con = new HashSet<>(); // 정상연결인지 아닌지 판단하기 위해 만든 set
 			
 			// true에 해당하는 지역과 false에 해당하는 지역 add
-			for (int i = 0; i < S_V.length; i++) {
-				if (S_V[i] == true) t_list.add(i+1);
-				if (S_V[i] == false) f_list.add(i+1);
+			for (int i = 0; i < D_V.length; i++) {
+				if (D_V[i] == true) t_list.add(i+1);
+				if (D_V[i] == false) f_list.add(i+1);
 			}
 			
 			// 한곳에 몰빵일때 종료
 			if (t_list.size() == N || f_list.size() == N) return; 
 			
-			// true와 false의 인구수 계산
-			for (int i = 0; i < S_V.length; i++) {
-				if(S_V[i] == true) t_sum += population[i];
-				if(S_V[i] == false) f_sum += population[i];
-			}
-			
 			// true에 속한 지역들 dfs 실행 
-			for (int i = 0; i < t_list.size(); i++) {
-				V = new boolean[N+1]; // 정점 방문 배열
-				dfs(t_list.get(i),i);
-			}
+			V = new boolean[N+1]; // 정점 방문 배열
+			dfs(t_list.get(0));
 			
 			// false에 속한 지역들 dfs 실행 
-			for (int i = 0; i < f_list.size(); i++) {
-				V = new boolean[N+1]; // 정점 방문 배열
-				dfs(f_list.get(i),i);
-			}
+			V = new boolean[N+1]; // 정점 방문 배열
+			dfs(f_list.get(0));
 			
-			// 만약 con의크기가 정점의 개수와 동일하면 정상적으로 연결되있는 상태, 아니면 비정상
+			// 만약 con의크기가 정점의 개수와 동일하면 정상적으로 연결되있는 상태
 			if (con.size()==N) {
-				flag = true; // 현재 진행중인 부분집합이 정상일때
-				check_f = true; // flag가 한번이라도 정상이면 정상이기때문에 필요한 변수
-			}else {
-				flag = false; // 현재 진행중인 부분집합이 비정상일때
-			}
-			
-			// 부분집합이 정상이면 ans 값 변화줌
-//			if (flag==true && t_list.size() == 0 && f_list.size() == 0) { 
-			if (flag==true) { 
+				flag = true; // 현재 진행중인 부분집합이 정상 연결 상태
+				
+				// true와 false의 인구수 계산
+				for (int i = 0; i < D_V.length; i++) {
+					if(D_V[i] == true) t_sum += population[i];
+					if(D_V[i] == false) f_sum += population[i];
+				}
+				
 				ans = Math.min(ans, Math.abs(t_sum - f_sum));
 			}
+			
 			return;
 		}
 		
-		S_V[index] = true;
+		D_V[index] = true;
 		subset(index+1);
-		S_V[index] = false;
+		D_V[index] = false;
 		subset(index+1);
 	}
 
-	private static void dfs(int index,int cnt) {
+	private static void dfs(int index) {
 		V[index] = true;
-		// dfs 첫번째 실행일때 -> dfs는 완탐이므로 지역구 하나당 한번씩 돌린결과 con 리스트에 저장
-		if (cnt == 0) {
-			con.add(index);
-		}
-//		t_list.remove(Integer.valueOf(index));
-//		f_list.remove(Integer.valueOf(index));
+		// dfs 실행 -> dfs는 완탐이므로 지역구 하나당 한번씩 돌린결과 con SET에 저장
+		con.add(index);
 		
 		// 지역구가 true일때
-		if(S_V[index-1] == true) {
+		if(D_V[index-1] == true) {
 			for (int i = 1; i <= N; i++) {
 				// 지역구가 true고 방문하지 않았고 인접행렬 1일때
-				if (S_V[i-1] == true && V[i] == false && map[index][i] == 1) {
-					dfs(i,cnt);
+				if (D_V[i-1] == true && V[i] == false && graph[index][i] == 1) {
+					dfs(i);
 				}
 			}
 		// 지역구가 false일때
-		}else if (S_V[index-1] == false) {
+		}else if (D_V[index-1] == false) {
 			for (int i = 1; i <= N; i++) {
 				// 지역구가 false고 방문하지 않았고 인접행렬 1일때
-				if (S_V[i-1] == false && V[i] == false && map[index][i] == 1) {
-					dfs(i,cnt);
+				if (D_V[i-1] == false && V[i] == false && graph[index][i] == 1) {
+					dfs(i);
 				}
 			}
 		}
--- Coding_Test/src/A_Test/Main_B_17471_게리멘더링2.java
+++ Coding_Test/src/A_Test/Main_B_17471_게리멘더링2.java
@@ -4,52 +4,17 @@ import java.util.*;
 import java.io.*;
 
 public class Main_B_17471_게리멘더링2 {
-	
-	static int n;
-	static int [] num;
-	static int [][] graph;
-	static boolean [] v;
-	
+
 
 	public static void main(String[] args) throws Exception{
-		System.setIn(new FileInputStream("input/17471.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(br.readLine());
-		
-		num = new int [n];
-		graph = new int [n+1][n+1];
-		v = new boolean [n+1];
- 		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
 
-		
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int range = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < range; j++) {
-				graph[i][Integer.parseInt(st.nextToken())] = 1;
-			}
-		}
-		
-		print(graph);
+
 		
 	}
 
 
-	private static void print(int[][] graph) {
-		for (int [] data_arr : graph) {
-			for (int data : data_arr) {
-				System.out.printf("%d ",data);
-			}
-			System.out.println();
-		}
-	}
+
+
 
 
 }
