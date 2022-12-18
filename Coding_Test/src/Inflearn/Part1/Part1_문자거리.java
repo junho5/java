b0abcaf139e88f446d077db3a38825168972016e
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-12-18 23:52:17 +0900

    1218

---
+++ Coding_Test/src/Inflearn/Part1/Part1_문자거리.java
@@ -0,0 +1,16 @@
+package Inflearn.Part1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Part1_문자거리 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+
+	}
+}
