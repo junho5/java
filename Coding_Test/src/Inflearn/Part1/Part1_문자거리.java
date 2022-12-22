6a417ea4096b75253821a5ff8adef2c4dcea6f6d
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2022-12-22 22:54:03 +0900

    1222

--- Coding_Test/src/Inflearn/Part1/Part1_문자거리.java
+++ Coding_Test/src/Inflearn/Part1/Part1_문자거리.java
@@ -1,16 +1,42 @@
 package Inflearn.Part1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Part1_문자거리 {
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
+		Scanner sc = new Scanner(System.in);
 
+		String inputString = sc.next();
+		char inputSearchString = sc.next().charAt(0);
+
+		char[] chars = inputString.toCharArray();
+		int[] result = new int[chars.length];
+		int tmp = 1000;
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i] == inputSearchString) {
+				tmp = 0;
+			} else {
+				tmp++;
+			}
+			result[i] = tmp;
+		}
+
+		tmp = 1000;
+		for (int j = chars.length - 1; j >= 0; j--) {
+			if (chars[j] == inputSearchString) {
+				tmp = 0;
+			} else {
+				tmp++;
+				result[j] = Math.min(result[j], tmp);
+			}
+
+		}
+
+		for (int k = 0; k < chars.length; k++) {
+			System.out.print(result[k] + " ");
+		}
 	}
+
 }
